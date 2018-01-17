00e2ead0b3543f6aba68ed74cee912456e29d770
martinvicknair <32040730+martinvicknair@users.noreply.github.com>
2018-01-16 18:40:41 -0700

    relogging userX, userY in firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -137,6 +137,8 @@ $(document).ready(function() {
       searchResult: searchResult,
       searchTerms: searchTerms,
       searchDate: searchDate,
+      userX: userX,
+      userY: userY,
 
     });
   }
