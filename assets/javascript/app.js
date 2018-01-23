35f18b8743cb69ef037b1222c0585d2a68c4cffc
Heath <heathrost@gmail.com>
2018-01-22 18:55:14 -0700

    fixed distance func

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,7 +11,6 @@ $(document).ready(function() {
 
   //search button was clicked, get the location
   $("#find-me").on("click", function() {
-    console.log("search was clicked");
     //get the location
     getLocation();
     listingArr = [];
@@ -35,7 +34,6 @@ $(document).ready(function() {
   function showPosition(position) {
     userX = position.coords.longitude;
     userY = position.coords.latitude;
-
     //firebase LOG where the user is searching from
     var database = firebase.database();
     database.ref().push({
@@ -43,10 +41,6 @@ $(document).ready(function() {
       userY: userY,
       dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
-
-    //creates the map on the page
-    // initMap();
-
     //find the sites!
     findSites();
   }
@@ -55,6 +49,7 @@ $(document).ready(function() {
   // api from https://services1.arcgis.com/RLQu0rK7h4kbsBq5/ArcGIS/rest/services
   // https://services1.arcgis.com/RLQu0rK7h4kbsBq5/ArcGIS/rest/services/Summer_Meal_Sites_2017/FeatureServer/0/query
   function findSites() {
+    var milesCalc = '.3';
     queryURL = "https://services1.arcgis.com/RLQu0rK7h4kbsBq5/ArcGIS/rest/services/Summer_Meal_Sites_2017/FeatureServer/0/query?geometry=%7Bx%3A" + userX + "%2C+y%3A" + userY +
       "%7D&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&distance=" + radius +
       ".&units=esriSRUnit_StatuteMile&returnGeodetic=false&outFields=siteName%2CsponsoringOrganization%2C+address%2CcontactPhone%2CstartDate%2C+endDate%2C+daysofOperation%2C+breakfastTime%2C+lunchTime%2C+snackTime%2C+dinnerSupperTime&returnGeometry=true&multipatchOption=xyFootprint&resultRecordCount=" +
@@ -82,69 +77,59 @@ $(document).ready(function() {
         listingArrTitle.push(name);
         listingArr.push(listObj);
 
-        //insert code for calculating distance from LatLng to the x and y of the location
-          milesCalc = mileCalc();
-          //insert code for calculating distance from LatLng to the x and y of the location
-            function mileCalc() {
-              jQuery.ajaxPrefilter(function(options) {
-    if (options.crossDomain && jQuery.support.cors) {
-        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-    }
-});
-              queryURL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins="
-               + userY + "," + userX
-               + "&destinations=" + results[i].geometry.y + "," + results[i].geometry.x
-               + "&key=AIzaSyAsbVYGpHF47lZVZHMEsHQuJQXffqQFt-w";
-              // console.log(queryURL);
-              $.ajax({
-                url: queryURL,
-                method: 'GET'
-              }).done(function(response) {
-                // console.log(response);
-                console.log(response.rows[0].elements[0].distance.text);
-                return milesCalc;
-                // console.log(milesCalc);
-              })
-            }; // end function mileCalc();
+        //cors fix
+        jQuery.ajaxPrefilter(function(options) {
+          if (options.crossDomain && jQuery.support.cors) {
+              options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+            }
+          });
 
-//      obj = JSON.parse(response);
-//      results = obj.features;
+        //calc the distance
+        queryURL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins="
+         + userY + "," + userX
+         + "&destinations=" + results[i].geometry.y + "," + results[i].geometry.x
+         + "&key=AIzaSyAsbVYGpHF47lZVZHMEsHQuJQXffqQFt-w";
 
-        if (lunchTime == null) {
-          lunchTime = 'not serving lunch';
-        }
-        if (breakfastTime == null) {
-          breakfastTime = 'not serving breakfast';
-        }
+        $.ajax({
+          url: queryURL,
+          method: 'GET'
+        }).done(function(response) {
+          milesCalc = response.rows[0].elements[0].distance.text;
 
-        listing = '<li align="center" class="list-group-item spec"><strong><h1 align="center">' + (i+1) + '</h1><h3>' + name + '</h3></strong>'
-        + 'Serving on: ' + days + '<br>'
-        + 'Breakfast Time: ' + breakfastTime + '<br>'
-        + 'Lunch Time: ' + lunchTime + '<br>'
-        + '<h4>' + milesCalc + ' miles away</h4>'
-        + '<a href="https://www.google.com/maps/search/?api=1&query=' + encAddress + '"><h4>' + address + '</h4></a>'
-        + '<a href="tel:/1' + phone + '"><h4>' + contact + '</h4></a></li>';
+          //build the primary array with all the info
+          if (lunchTime == null) {
+            lunchTime = 'not serving lunch';
+          }
+          if (breakfastTime == null) {
+            breakfastTime = 'not serving breakfast';
+          }
 
-        listingMASTER.push(listing);
-        $("#listings-area").append(listing);
-      }
+          listing = '<li align="center" class="list-group-item spec"><strong><h1 align="center">' + (i+1) + '</h1><h3>' + name + '</h3></strong>'
+          + 'Serving on: ' + days + '<br>'
+          + 'Breakfast Time: ' + breakfastTime + '<br>'
+          + 'Lunch Time: ' + lunchTime + '<br>'
+          + '<h4>' + milesCalc + ' miles away</h4>'
+          + '<a href="https://www.google.com/maps/search/?api=1&query=' + encAddress + '"><h4>' + address + '</h4></a>'
+          + '<a href="tel:/1' + phone + '"><h4>' + contact + '</h4></a></li>';
 
-      addPoints(listingArr, listingArrTitle, listingMASTER);
-     
+          listingMASTER.push(listing);
+
+          //update the text beneath the map on the screen
+          $("#listings-area").append(listing);
+          //add the points + data to the map!
+          addPoints(listingArr, listingArrTitle, listingMASTER);
+        });
+      };
     });
   };
 
   function addPoints(listingArr, listingArrTitle, listingMASTER) {
-    console.log('addpoints function yo')
-    console.log(listingArrTitle[0])
-    
     var yourLocation = { lat: userY, lng: userX };
-    
     var map = new google.maps.Map(document.getElementById("map"), {
       zoom: 12,
       center: yourLocation
     });
-   
+
     var marker = new google.maps.Marker({
       position: yourLocation,
       map: map,
@@ -154,39 +139,29 @@ $(document).ready(function() {
 
     for(i=0; i<listingArr.length; i++) {
       addMarker(listingArr[i], map, listingArrTitle[i], i, listingMASTER[i]);
-    } 
-   
-    // addMarker(yourLocation, map);
-    // var yourLocation2 = { lat: 39.6739293170005,  lng: - 104.95076490099969 }
-    // addMarker(yourLocation2, map);
+    }
   };
 
-
+  //adds a marker
   function addMarker(location, map, title, label, master) {
-    console.log(label)
     label = label + 1;
     // Add the marker at the clicked location, and add the next-available label
-    // from the array of alphabetical characters.
     var marker = new google.maps.Marker({
       position: location,
       map: map,
       title: title,
       label: encodeURIComponent(label)
-      // adding code for info window 
+      // adding code for info window
       //https://stackoverflow.com/questions/11507187/add-infowindow-to-looped-array-of-markers-on-google-map-using-api-v3
-     //clickable: true 
     });
-    // adding code for info window 
+    // adding code for info window
     marker.info = new google.maps.InfoWindow({
-    content: master
-  });
-    google.maps.event.addListener(marker, 'click', function() {
-    marker.info.open(map, this); 
+      content: master
     });
-  
+    google.maps.event.addListener(marker, 'click', function() {
+      marker.info.open(map, this);
+    });
   };
-
-  //other stuff! :-D
 
   // from https://stackoverflow.com/questions/8358084/regular-expression-to-reformat-a-us-phone-number-in-javascript
   // mobile hyperlink attribute to place a call is href="tel:/13038005415"
@@ -194,19 +169,5 @@ $(document).ready(function() {
     var s2 = ("" + s).replace(/\D/g, '');
     var m = s2.match(/^(\d{3})(\d{3})(\d{4})$/);
     return (!m) ? null : m[1] + "-" + m[2] + "-" + m[3];
-  }
-
-  //returns human readable semi-anonymous place location for user
-  // function getUserLoc() {
-  //
-  //   queryURL = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + userY + ',' + userX + "&key=AIzaSyCm9jtLUThY_3O1Uec3Ao-DWDkkBNzCl3U";
-  //   $.ajax({
-  //     url: queryURL,
-  //     method: 'GET'
-  //   }).done(function(response) {
-  //     userLoc = response.results[1].formatted_address;
-  //   });
-  // };
-
-
+  };
 });
